aa4d382fa74741f47f09c8a427e1c914b7938456
mourad132 <58851960+mourad132@users.noreply.github.com>
2022-08-16 13:48:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -430,7 +430,7 @@ app.delete("/delete/:type/:id", (req, res) => {
 				console.log(err)
 			} else {
 				//then redirect to the home page again
-				res.redirect("/home#" + req.params.type)
+				res.redirect("/files/" + found.image)
 			}
 		})
 	} else if(req.params.type == "complain"){
@@ -438,7 +438,7 @@ app.delete("/delete/:type/:id", (req, res) => {
 			if(err){
 				console.log(err)
 			} else {
-				res.redirect("/home#" + req.params.type)
+				res.redirect("/files/" + found.image)
 			}
 		})
 	} else if(req.params.type == "service"){
@@ -446,7 +446,7 @@ app.delete("/delete/:type/:id", (req, res) => {
 			if(err){
 				console.log(err)
 			} else {
-				res.redirect("/home#" + req.params.type)
+				res.redirect("/files/" + found.image)
 			}
 		})
 	} else if(req.params.type == "event"){
@@ -454,7 +454,7 @@ app.delete("/delete/:type/:id", (req, res) => {
 			if(err){
 				console.log(err)
 			} else {
-				res.redirect("/home#" + req.params.type)
+				res.redirect("/files/" + found.image)
 			}
 		})
 	} else if(req.params.type == "post"){
@@ -462,7 +462,7 @@ app.delete("/delete/:type/:id", (req, res) => {
 			if(err){
 				console.log(err)
 			} else {
-				res.redirect("/home#" + req.params.type)
+				res.redirect("/files/" + found.image)
 			}
 		})
 	} else {
@@ -936,13 +936,13 @@ app.post('/new/event', (req, res) => {
 })
 
 //Delete Files Route
-app.delete('/files/:id', (req, res) => {
-  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+app.get('/files/:filename', (req, res) => {
+  gfs.remove({ filename: req.params.filename, root: 'uploads' }, (err, gridStore) => {
     if (err) {
       return res.status(404).json({ err: err });
     }
 	//redirects to the upload photo profile
-    res.redirect('/photo');
+    res.redirect('/home');
   });
 });
 
