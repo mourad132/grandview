c7181265f3bbdcb7b9382be8c125ab8607870399
mourad132 <58851960+mourad132@users.noreply.github.com>
2022-08-16 13:36:15 +0200

    Update post.js

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ var schema = new mongoose.Schema({
     title: String,
     post: String,
 	author: {username: String, image: String, id: String},
-	comments: [{username: String, created: String, createdTime: String, body: String, image: String}],
+	comments: [{username: String, created: String, createdTime: String, body: String, image: String, authorId: String}],
 	created: String,
 	createdTime: String,
 });
